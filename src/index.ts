7d862ddabf3086ce04db5198d219a8a5669f1917
minotogna <mino.togna@gmail.com>
2021-02-22 15:01:53 +0100

    remove common folder

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export type { AuthGroup } from './common/authGroup'
-export type { Cycle } from './common/cycle'
-export type { Labels } from './common/labels'
-export type { SRS } from './common/srs'
-export type { Validation } from './common/validation'
+export type { AuthGroup } from './auth'
+export type { Cycle } from './cycle'
+export type { Labels } from './labels'
+export type { SRS } from './srs'
+export type { Validation } from './validation'
 
 export type { Survey } from './survey'
 export type { SurveyInfo } from './survey/info'
