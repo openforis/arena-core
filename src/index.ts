7784738c8ad03e788d95ef10a607924d05d37f03
sorja <ping@mirosorja.com>
2021-02-23 15:53:24 +0200

    separate enum export

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,8 @@ export type { Labels } from './labels'
 export type { SRS } from './srs'
 export type { Survey } from './survey'
 export type { SurveyInfo } from './survey/info'
-export type { UserStatus, UserSurvey, UserSurveys, UserPrefs, UserTitle, UserProps, User } from './auth/user'
+export type { UserSurvey, UserSurveys, UserPrefs, UserProps, User } from './auth/user'
 export type { Validation } from './validation'
+
+// Enums
+export { UserStatus, UserTitle } from './auth/user'
