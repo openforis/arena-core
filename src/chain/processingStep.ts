241d0c83fe77e6b625b11dcf9515c38bba8c2720
sorja <ping@mirosorja.com>
2021-02-25 15:20:02 +0200

    Update processing step

--- src/chain/processingStep.ts
+++ src/chain/processingStep.ts
@@ -1,13 +1,17 @@
 import { ArenaObject } from 'src/common'
+import { ProcessingStepCalculation } from './processingStepCalculation'
 
 export interface ProcessingStepProps {
-    entityUuid: string
-    categoryUuid?: string
-}
-
-export interface ProcessingStep extends ArenaObject<ProcessingStepProps> {
   entityUuid?: string
   categoryUuid?: string
   virtual: boolean
   variablesPreviousStep: string
+}
+
+export interface ProcessingStep extends ArenaObject<ProcessingStepProps> {
+  calculations: Array<ProcessingStepCalculation>
+  calculationUuids: Array<string>
+  index: number
+  processingChainUuid: string
+  temporary: boolean
 }
