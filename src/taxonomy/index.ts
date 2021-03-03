2ee1c325ba76901fcd9b52f8ce79b396f0bfc223
Miró Sorja <ping@mirosorja.com>
2021-03-03 15:36:08 +0200

    add taxonomy service (#16)
    * add taxonomy service
    
    * add taxonservice
    
    * fix typo
    
    * update taxonomy
    
    * surveyId as number
    
    * remove unused import
    
    * get,getMany taxonService
    
    * update taxonomy service
    
    * fix typo
    
    Co-authored-by: Mino Togna <minotogna@users.noreply.github.com>

--- src/taxonomy/index.ts
+++ src/taxonomy/index.ts
@@ -3,3 +3,7 @@ export type { Taxonomy, TaxonomyProps } from './taxonomy'
 export type { Taxon, TaxonProps } from './taxon'
 
 export type { VernacularName, VernacularNameProps } from './taxonVernacularName'
+
+export type { TaxonomyService } from './service'
+
+export type { TaxonService } from './taxonService'
