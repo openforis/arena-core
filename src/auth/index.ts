e0baab8c0d09af5a17663c8e6f2b69b55d70c2dc
sorja <ping@mirosorja.com>
2021-03-05 10:13:50 +0200

    add userService

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,6 +1,7 @@
 export type { AuthGroup } from './authGroup'
 
-export { Permission } from './permission'
-
 export type { User, UserPrefs, UserProps, UserPrefSurveys } from './user'
+export type { UserService } from './userService'
+
 export { UserStatus, UserTitle } from './user'
+export { Permission } from './permission'
