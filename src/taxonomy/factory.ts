77314901fa9c5e6d9e33cf896dea79c07afc4790
ramirobg94 <ramirobg94@gmail.com>
2021-03-09 23:33:37 +0100

    Factory/vernacularNames

--- src/taxonomy/factory.ts
+++ src/taxonomy/factory.ts
@@ -2,6 +2,9 @@ import { v4 as uuidv4 } from 'uuid'
 
 import { Factory } from 'src/common'
 import { Taxonomy, TaxonomyProps } from './taxonomy'
+import { Taxon, TaxonProps } from './taxon'
+import { VernacularName } from './taxonVernacularName'
+import { LanguageCode } from 'src/language'
 
 export type TaxonomyFactoryParams = {
   props?: TaxonomyProps
@@ -13,8 +16,8 @@ export const TaxonomyFactory: Factory<Taxonomy> = {
   createInstance: (params: TaxonomyFactoryParams): Taxonomy => {
     const defaultProps = {
       props: {},
-      published: undefined,
-      draft: undefined,
+      published: false,
+      draft: true,
     }
 
     const { props, draft, published } = {
@@ -27,6 +30,56 @@ export const TaxonomyFactory: Factory<Taxonomy> = {
       props,
       draft,
       published,
+    }
+  },
+}
+
+// Taxon
+export type TaxonFactoryParams = {
+  props: TaxonProps
+  taxonomyUuid: string
+  vernacularNames?: {
+    [key: string]: VernacularName[]
+  }
+}
+
+export const TaxonFactory: Factory<Taxon> = {
+  createInstance: (params: TaxonFactoryParams): Taxon => {
+    const defaultProps = {
+      props: {},
+      vernacularNames: {},
+    }
+
+    const { props, taxonomyUuid, vernacularNames } = {
+      ...defaultProps,
+      ...params,
+    }
+
+    return {
+      uuid: uuidv4(),
+      props,
+      taxonomyUuid,
+      vernacularNames,
+    }
+  },
+}
+
+// VernacularNameFactory
+export type VernacularNameParams = {
+  lang: LanguageCode
+  name: string
+}
+
+export const VernacularNameFactory: Factory<VernacularName> = {
+  createInstance: (params: VernacularNameParams): VernacularName => {
+    const { lang, name } = params
+
+    return {
+      uuid: uuidv4(),
+      props: {
+        lang,
+        name,
+      },
     }
   },
 }
--- src/taxonomy/taxonomy.test.ts
+++ src/taxonomy/taxonomy.test.ts
@@ -12,12 +12,14 @@ describe('taxonomyFactory', () => {
 
     expect(taxonomy).toHaveProperty('uuid')
     expect(taxonomy).toHaveProperty('draft')
+    expect(taxonomy.draft).toBe(true)
     expect(taxonomy).toHaveProperty('published')
+    expect(taxonomy.published).toBe(false)
 
     expect(taxonomy).toHaveProperty('props')
     expect(taxonomy.props.descriptions).toBeUndefined()
     expect(taxonomy.props).toHaveProperty('name')
-    expect(taxonomy.props.name).toBe('Taxonomy')
+    expect(taxonomy.props.name).toBe(taxonomyParams.props?.name)
     expect(taxonomy.props.vernacularLanguageCodes).toBeUndefined()
   })
 })
