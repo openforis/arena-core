e4307727556c5665c5442d6e18715ce7a2e5a954
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-03-09 16:13:51 +0100

    Expression evaluator: allow using global identifiers

--- src/expression/javascript/node/identifier.ts
+++ src/expression/javascript/node/identifier.ts
@@ -1,7 +1,13 @@
 import { ExpressionNodeEvaluator, IdentifierExpression } from '../../node'
+import { getGlobalObjectProperty } from '../global'
 
 export class IdentifierEvaluator extends ExpressionNodeEvaluator<IdentifierExpression> {
-  evaluate(): any {
-    throw new Error(`identifier not supported`)
+  evaluate(expressionNode: IdentifierExpression): any {
+    const { name } = expressionNode
+    const globalProp = getGlobalObjectProperty(name, this.context.expressionNode)
+    if (globalProp !== null) {
+      return globalProp
+    }
+    throw new Error(`Identifier not found: ${name}`)
   }
 }
