31ce3e39feb33d45b0c23513c7ffc4d0959ef471
sorja <ping@mirosorja.com>
2021-03-11 12:13:52 +0200

    getAuthGroupBySurveyUuid: update function return type

--- src/auth/users.ts
+++ src/auth/users.ts
@@ -1,17 +1,23 @@
 import { User } from './user'
-import { AuthGroup} from './authGroup'
+import { AuthGroup } from './authGroup'
 import { AuthGroups } from './authGroups'
 
-const getAuthGroups = (user:User):AuthGroup[] => user?.authGroups || []
+const getAuthGroups = (user: User): AuthGroup[] => user?.authGroups || []
 
-const isSystemAdmin =  (user:User):boolean => (getAuthGroups(user) || []).some(AuthGroups.isSystemAdminGroup)
+const isSystemAdmin = (user: User): boolean => (getAuthGroups(user) || []).some(AuthGroups.isSystemAdminGroup)
 
-const getAuthGroupBySurveyUuid= (surveyUuid?: string, includeSystemAdmin = true) => (user: User):AuthGroup => {
+const getAuthGroupBySurveyUuid = (surveyUuid?: string, includeSystemAdmin = true) => (
+  user: User
+): AuthGroup | undefined => {
   const authGroups = getAuthGroups(user)
-  return includeSystemAdmin && isSystemAdmin(user) && authGroups ? authGroups[0] : authGroups?.find(group => group.surveyUuid === surveyUuid)  
+  if (includeSystemAdmin && isSystemAdmin(user) && authGroups) {
+    return authGroups[0]
+  }
+
+  return authGroups?.find((authGroup) => authGroup.surveyUuid === surveyUuid)
 }
 
 export const Users = {
   isSystemAdmin,
-  getAuthGroupBySurveyUuid
+  getAuthGroupBySurveyUuid,
 }
