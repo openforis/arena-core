c377817dc78c05391fd6b5fae005b3dc98afeb6d
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-03-15 17:27:30 +0100

    CU-ff5eq6 - Job

--- src/job/job.ts
+++ src/job/job.ts
@@ -1,26 +1,79 @@
 import { JobStatus } from './status'
 
-export abstract class Job<R> {
-  params: any // Execution parameters
-  context: any // Context object (shared among nested jobs)
-
+/**
+ * Asynchronous task hanlder.
+ *
+ * Status workflow:
+ * - pending
+ * - running
+ * - (end)
+ * -- succeed
+ * -- failed
+ * -- canceled
+ *
+ * Methods that can be overwritten by subclasses:
+ * - onStart
+ * - execute
+ * - beforeSuccess
+ * - beforeEnd
+ * - onEnd
+ */
+export interface Job<P, R, C> {
+  /**
+   * Execution parameters.
+   */
+  params?: P
+  /**
+   * Context object (shared among nested jobs).
+   */
+  context?: C
+  /**
+   * Identifier of the job.
+   */
   uuid: string
-  type: string // string representing the Job type
-
-  status = JobStatus
-  startTime: Date
-  endTime: Date
+  /**
+   * String representing the Job type.
+   */
+  type: string
+  /**
+   * Current status.
+   */
+  status: JobStatus
+  /**
+   * Execution start time.
+   */
+  startTime?: Date
+  /**
+   * Execution end time.
+   */
+  endTime?: Date
+  /**
+   * Number of total items to process.
+   */
   total: number
-
-  processed: number
-  result: R
-  errors: any
-  innerJobs: Array<Job<any>>
-  currentInnerJobIndex: number
-
-  eventListener: any
-
-  async shouldExecute(): Promise<boolean> {
-    return true
-  }
+  /**
+   * Number of processed items.
+   */
+  processed?: number
+  /**
+   * Result of the job.
+   */
+  result?: R
+  /**
+   * Errors (when status is failed)
+   */
+  errors?: any
+  /**
+   * Inner jobs (if any).
+   */
+  innerJobs?: Array<Job<any, any, any>>
+  /**
+   * Returns true if the job should be executed.
+   * It returns true by default.
+   */
+  shouldExecute(): boolean
+  /**
+   * Starts the execution of the job.
+   */
+  start(): void
 }
