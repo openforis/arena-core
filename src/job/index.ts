35e39c8f8332553359ce2d8008107c224d014346
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-03-15 18:23:19 +0100

    Job: use JobSummary in Job interface

--- src/job/index.ts
+++ src/job/index.ts
@@ -1,2 +1,3 @@
 export type { Job } from './job'
 export { JobStatus } from './status'
+export type { JobSummary } from './summary'
--- src/job/job.ts
+++ src/job/job.ts
@@ -1,4 +1,4 @@
-import { JobStatus } from './status'
+import { JobSummary } from './summary'
 
 /**
  * Asynchronous task hanlder.
@@ -10,14 +10,7 @@ import { JobStatus } from './status'
  * -- succeed
  * -- failed
  * -- canceled
- *
- * Methods that can be overwritten by subclasses:
- * - onStart
- * - execute
- * - beforeSuccess
- * - beforeEnd
- * - onEnd
- */
+ *  */
 export interface Job<P, R, C> {
   /**
    * Execution parameters.
@@ -28,45 +21,9 @@ export interface Job<P, R, C> {
    */
   context?: C
   /**
-   * Identifier of the job.
+   * Summary of the job with the information about its status.
    */
-  uuid: string
-  /**
-   * String representing the Job type.
-   */
-  type: string
-  /**
-   * Current status.
-   */
-  status: JobStatus
-  /**
-   * Execution start time.
-   */
-  startTime?: Date
-  /**
-   * Execution end time.
-   */
-  endTime?: Date
-  /**
-   * Number of total items to process.
-   */
-  total: number
-  /**
-   * Number of processed items.
-   */
-  processed?: number
-  /**
-   * Result of the job.
-   */
-  result?: R
-  /**
-   * Errors (when status is failed)
-   */
-  errors?: any
-  /**
-   * Inner jobs (if any).
-   */
-  innerJobs?: Array<Job<any, any, any>>
+  summary: JobSummary<R>
   /**
    * Returns true if the job should be executed.
    * It returns true by default.
