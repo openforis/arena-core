0ea0778e8319321e57d8558ce4551354c8b5ae68
Miró Sorja <ping@mirosorja.com>
2021-04-16 13:07:05 +0300

    Feature/fieldvalidator email (#75)
    * fieldValidators: add email
    
    * fieldValidators: add email

--- src/validation/fieldValidators/index.ts
+++ src/validation/fieldValidators/index.ts
@@ -1,7 +1,8 @@
+import { email } from './email'
 import { name } from './name'
 import { notKeyword } from './notKeyword'
 import { numeric } from './numeric'
 import { numericPositive } from './numericPositive'
 import { required } from './required'
 
-export const FieldValidators = { name, notKeyword, numeric, numericPositive, required }
+export const FieldValidators = { email, name, notKeyword, numeric, numericPositive, required }
