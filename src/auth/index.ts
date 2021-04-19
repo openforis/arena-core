a68c7b0b13639b67fefb853542a3ee938655c7d0
sorja <ping@mirosorja.com>
2021-04-19 10:41:41 +0300

    auth: export UserInvitation

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -2,7 +2,7 @@ export { Authorizer } from './authorizer'
 
 export type { AuthGroup } from './authGroup'
 
-export type { User, UserPrefs, UserProps, UserPrefSurveys } from './user'
+export type { User, UserPrefs, UserProps, UserPrefSurveys, UserInvitation } from './user'
 export type { UserService } from './userService'
 
 export { UserStatus, UserTitle } from './user'
