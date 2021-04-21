a62c63f337fa83828b7f4e2701f73c26f5336737
Miró Sorja <ping@mirosorja.com>
2021-04-21 14:24:32 +0300

    Feature/objects survey service (#82)
    * survey/service: backup?: boolean
    
    * objects: refactor to separate files, add camelize
    
    * camelize: Update jsdoc

---
+++ src/utils/objects/path.ts
@@ -0,0 +1,23 @@
+/**
+ * Extracts the value in the specified path from the specified object.
+ *
+ * @param {array|string} path - Array of path parts or string with parts concatenated with dots.
+ * @returns {any} Value at the specified path.
+ */
+export const path = (path: Array<string> | string) => (obj: any): any => {
+  if (!obj) return null
+
+  const parts = Array.isArray(path) ? path : path.split('.')
+  let current = obj
+  let i = 0
+
+  for (; i < parts.length; i++) {
+    const part = parts[i]
+    if (current[part] === undefined) {
+      return undefined
+    } else {
+      current = current[part]
+    }
+  }
+  return current
+}
