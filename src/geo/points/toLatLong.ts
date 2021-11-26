26a97e59595f1f0f4b59e01d607ed0cdc8432688
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2021-11-26 16:16:04 +0100

    added Points.transform function (#102)
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- src/geo/points/toLatLong.ts
+++ src/geo/points/toLatLong.ts
@@ -1,30 +1,5 @@
-import proj4 from 'proj4'
-
-import { DEFAULT_SRS, SRSs } from '../../srs'
+import { DEFAULT_SRS } from '../../srs'
 import { Point } from '../point'
-import { PointFactory } from '../pointFactory'
-import { isFilled } from './isFilled'
+import { transform } from './transform'
 
-export const toLatLong = (point: Point): Point | null => {
-  if (!isFilled(point)) return null
-
-  const { x, y, srs } = point
-
-  if (srs === DEFAULT_SRS.code) {
-    // projection is not needed
-    return point
-  }
-
-  const srsFrom = SRSs.getSRSByCode(srs)
-  if (!srsFrom) return null
-
-  const srsTo = DEFAULT_SRS
-
-  try {
-    const [long, lat] = proj4(srsFrom.wkt, srsTo.wkt, [x, y])
-
-    return PointFactory.createInstance({ srs: DEFAULT_SRS.code, x: long, y: lat })
-  } catch (error) {
-    return null
-  }
-}
+export const toLatLong = (point: Point): Point | null => transform(point, DEFAULT_SRS.code)
