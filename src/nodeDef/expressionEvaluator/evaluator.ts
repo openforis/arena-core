61a763ab1a643c4eacb2290cf9ca0e109fc478a3
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-05-12 12:36:11 +0200

    updated survey dependencies

--- src/nodeDef/expressionEvaluator/evaluator.ts
+++ src/nodeDef/expressionEvaluator/evaluator.ts
@@ -3,6 +3,23 @@ import { NodeDefIdentifierEvaluator } from './node/identifier'
 import { NodeDefMemberEvaluator } from './node/member'
 import { nodeDefExpressionFunctions } from './functions'
 import { NodeDefExpressionContext } from './context'
+import { Survey, Surveys } from '../../survey'
+import { NodeDef, NodeDefProps, NodeDefType } from '../nodeDef'
+import { SurveyDependencyType } from '../../survey/survey'
+
+const isContextParentByDependencyType = {
+  [SurveyDependencyType.applicable]: true,
+  [SurveyDependencyType.defaultValues]: false,
+  [SurveyDependencyType.formula]: false,
+  [SurveyDependencyType.validations]: false,
+}
+
+const selfReferenceAllowedByDependencyType = {
+  [SurveyDependencyType.applicable]: false,
+  [SurveyDependencyType.defaultValues]: false,
+  [SurveyDependencyType.formula]: false,
+  [SurveyDependencyType.validations]: true,
+}
 
 export class NodeDefExpressionEvaluator extends JavascriptExpressionEvaluator<NodeDefExpressionContext> {
   constructor() {
@@ -12,12 +29,28 @@ export class NodeDefExpressionEvaluator extends JavascriptExpressionEvaluator<No
     })
   }
 
-  findReferencedNodeDefUuids(expression: string, context: NodeDefExpressionContext): Set<string> {
+  findReferencedNodeDefUuids(params: {
+    survey: Survey
+    nodeDef: NodeDef<NodeDefType, NodeDefProps>
+    expression: string
+    dependencyType: SurveyDependencyType
+  }): Set<string> {
+    const { survey, nodeDef, expression, dependencyType } = params
+    const isContextParent = isContextParentByDependencyType[dependencyType]
+    const selfReferenceAllowed = selfReferenceAllowedByDependencyType[dependencyType]
+    const nodeDefContext = isContextParent ? Surveys.getNodeDefParent({ survey, nodeDef }) : nodeDef
+    const context = {
+      survey,
+      nodeDefContext,
+      nodeDefCurrent: nodeDef,
+      selfReferenceAllowed,
+      referencedNodeDefUuids: new Set<string>(),
+    }
     try {
       this.evaluate(expression, context)
-      return context.referencedNodeDefUuids || new Set()
+      return context.referencedNodeDefUuids || new Set<string>()
     } catch (error) {
-      return new Set()
+      return new Set<string>()
     }
   }
 }
