14f77c0f4c5825d484bed201ce35aade80312bdb
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2022-07-12 10:24:22 +0200

    Moved Records.removeNode function to RecordNodesUpdater (#137)
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- src/record/recordNodesUpdater/index.ts
+++ src/record/recordNodesUpdater/index.ts
@@ -1,9 +1,12 @@
 import { createNodeAndDescendants } from './recordNodesCreator'
 import { updateNodesDependents } from './recordNodesUpdater'
+import { removeNode, removeNodes } from './recordNodesDeleter'
 
 export { RecordUpdateResult } from './recordUpdateResult'
 
 export const RecordNodesUpdater = {
   createNodeAndDescendants,
   updateNodesDependents,
+  removeNode,
+  removeNodes,
 }
