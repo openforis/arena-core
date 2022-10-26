1991b2be45156259658e91a2a4ae0136bd45fef1
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-10-26 13:32:36 +0200

    Record updater: added tests (validation rules)

--- src/record/recordUpdater.test.ts
+++ src/record/recordUpdater.test.ts
@@ -11,9 +11,53 @@ import { User } from '../auth'
 import { Records } from './records'
 import { Record } from './record'
 import { Validations } from '../validation/validations'
-import { Surveys } from '../survey'
+import { Survey } from '../survey'
 
 let user: User
+
+const updateAttributeAndExpectValidation = async (params: {
+  survey: Survey
+  record: Record
+  nodePath: string
+  value: any
+  validationNodePath?: string
+  expectedFieldValidation: boolean
+  expectedValidationFieldsSize: number
+}): Promise<Record> => {
+  const {
+    survey,
+    record,
+    nodePath,
+    value,
+    validationNodePath = params.nodePath,
+    expectedFieldValidation,
+    expectedValidationFieldsSize,
+  } = params
+  const nodeToUpdate = TestUtils.getNodeByPath({ survey, record, path: nodePath })
+
+  const nodeUpdated = { ...nodeToUpdate, value }
+  const recordUpdated = Records.addNode(nodeUpdated)(record)
+
+  const updateResult = await RecordUpdater.updateNode({
+    survey,
+    record: recordUpdated,
+    node: nodeUpdated,
+  })
+
+  expect(updateResult).not.toBeNull()
+
+  const validation = Validations.getValidation(updateResult.record)
+
+  const validationNode = TestUtils.getNodeByPath({ survey, record, path: validationNodePath })
+  const expectedFieldValidationNodeUuid = validationNode?.uuid
+
+  const fieldValidation = Validations.getFieldValidation(expectedFieldValidationNodeUuid)(validation)
+
+  expect(fieldValidation.valid).toBe(expectedFieldValidation)
+  expect(Object.values(Validations.getFieldValidations(validation)).length).toBe(expectedValidationFieldsSize)
+
+  return updateResult.record
+}
 
 describe('Record updater - required attribute', () => {
   beforeAll(async () => {
@@ -23,87 +67,44 @@ describe('Record updater - required attribute', () => {
   test('Validation: required value', async () => {
     const survey = new SurveyBuilder(
       user,
-      entityDef('root_entity', integerDef('identifier').key(), textDef('remarks'))
+      entityDef(
+        'root_entity',
+        integerDef('identifier').key(),
+        integerDef('int_attr').validationExpressions('this > 10'),
+        textDef('remarks')
+      )
     ).build()
 
     let record = new RecordBuilder(
       user,
       survey,
-      entity('root_entity', attribute('identifier', 10), attribute('remarks', 'Some remarks'))
+      entity(
+        'root_entity',
+        attribute('identifier', 10),
+        attribute('int_attr', 20),
+        attribute('remarks', 'Some remarks')
+      )
     ).build()
 
-    const updateAttributeAndExpectValidation = async (params: {
-      nodePath: string
-      value: any
-      validationNodePath?: string
-      expectedFieldValidation: boolean
-      expectedValidationFieldsSize: number
-    }): Promise<Record> => {
-      const {
-        nodePath,
-        value,
-        validationNodePath = params.nodePath,
-        expectedFieldValidation,
-        expectedValidationFieldsSize,
-      } = params
-      const nodeToUpdate = TestUtils.getNodeByPath({ survey, record, path: nodePath })
-
-      const nodeUpdated = { ...nodeToUpdate, value }
-      const recordUpdated = Records.addNode(nodeUpdated)(record)
-
-      const updateResult = await RecordUpdater.updateNode({
-        survey,
-        record: recordUpdated,
-        node: nodeUpdated,
-      })
-
-      expect(updateResult).not.toBeNull()
-
-      const validation = Validations.getValidation(updateResult.record)
-
-      const validationNode = TestUtils.getNodeByPath({ survey, record, path: validationNodePath })
-      const expectedFieldValidationNodeUuid = validationNode?.uuid
-
-      const fieldValidation = Validations.getFieldValidation(expectedFieldValidationNodeUuid)(validation)
-
-      if (fieldValidation.valid !== expectedFieldValidation) {
-        const nodeDef = Surveys.getNodeDefByUuid({ survey, uuid: validationNode.nodeDefUuid })
-        throw new Error(
-          '' +
-            JSON.stringify(updateResult.record.validation) +
-            '\n' +
-            JSON.stringify(updateResult.validation) +
-            '\n' +
-            fieldValidation.valid +
-            JSON.stringify(fieldValidation) +
-            ' val ' +
-            expectedFieldValidationNodeUuid +
-            ' ' +
-            nodeDef?.props?.name +
-            ' ' +
-            JSON.stringify(updateResult.validation)
-        )
-      }
-
-      expect(fieldValidation.valid).toBe(expectedFieldValidation)
-      expect(Object.values(Validations.getFieldValidations(validation)).length).toBe(expectedValidationFieldsSize)
-
-      return updateResult.record
-    }
-
     record = await updateAttributeAndExpectValidation({
+      survey,
+      record,
       nodePath: 'root_entity.identifier',
       value: null,
       expectedFieldValidation: false,
       expectedValidationFieldsSize: 1,
     })
     record = await updateAttributeAndExpectValidation({
+      survey,
+      record,
       nodePath: 'root_entity.identifier',
       value: 10,
       expectedFieldValidation: true,
       expectedValidationFieldsSize: 0,
     })
     record = await updateAttributeAndExpectValidation({
+      survey,
+      record,
       nodePath: 'root_entity.identifier',
       value: null,
       expectedFieldValidation: false,
@@ -112,11 +113,54 @@ describe('Record updater - required attribute', () => {
 
     // test that changing another attribute does not affect existing attribute validations
     record = await updateAttributeAndExpectValidation({
+      survey,
+      record,
       nodePath: 'root_entity.remarks',
       value: null,
       validationNodePath: 'root_entity.identifier',
       expectedFieldValidation: false,
       expectedValidationFieldsSize: 1,
     })
+
+    expect(record).not.toBeNull()
+  })
+
+  test('Validation: attribute validation expression', async () => {
+    const survey = new SurveyBuilder(
+      user,
+      entityDef(
+        'root_entity',
+        integerDef('identifier').key(),
+        integerDef('int_attr').validationExpressions('this > 10')
+      )
+    ).build()
+
+    let record = new RecordBuilder(
+      user,
+      survey,
+      entity('root_entity', attribute('identifier', 10), attribute('int_attr', 20))
+    ).build()
+
+    record = await updateAttributeAndExpectValidation({
+      survey,
+      record,
+      nodePath: 'root_entity.int_attr',
+      value: 9,
+      validationNodePath: 'root_entity.int_attr',
+      expectedFieldValidation: false,
+      expectedValidationFieldsSize: 1,
+    })
+
+    record = await updateAttributeAndExpectValidation({
+      survey,
+      record,
+      nodePath: 'root_entity.int_attr',
+      value: 11,
+      validationNodePath: 'root_entity.int_attr',
+      expectedFieldValidation: true,
+      expectedValidationFieldsSize: 0,
+    })
+
+    expect(record).not.toBeNull()
   })
 })
