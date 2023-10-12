81dc0611a500eccb8e13e01669b0fdfcdecd59e0
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2023-10-12 11:04:59 +0200

    fixed Points.isFilled function (#206)
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- src/geo/points/isFilled.ts
+++ src/geo/points/isFilled.ts
@@ -2,4 +2,4 @@ import { Objects } from '../../utils'
 import { Point } from '../point'
 
 export const isFilled = (point: Point): boolean =>
-  !Objects.isEmpty(point.srs) && !Objects.isEmpty(point.x) && !Objects.isEmpty(point.y)
+  !Objects.isEmpty(point) && !Objects.isEmpty(point.srs) && !Objects.isEmpty(point.x) && !Objects.isEmpty(point.y)
