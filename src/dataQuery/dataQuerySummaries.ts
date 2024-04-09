85cb1d7970f3a28597de504e85fc1263d9d546fc
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-04-09 14:02:19 +0200

    handle nulls in data query objects

--- src/dataQuery/dataQuerySummaries.ts
+++ src/dataQuery/dataQuerySummaries.ts
@@ -10,20 +10,20 @@ const PropKeys = {
 
 const create = ({ content = undefined }): DataQuerySummary => ({ uuid: UUIDs.v4(), content })
 
-const getUuid = (querySummary: DataQuerySummary): string => querySummary.uuid ?? ''
+const getUuid = (querySummary: DataQuerySummary): string => querySummary?.uuid ?? ''
 
-const getName = (querySummary: DataQuerySummary): string => querySummary.props?.name ?? ''
+const getName = (querySummary: DataQuerySummary): string => querySummary?.props?.name ?? ''
 
-const getContent = (querySummary: DataQuerySummary): DataQuery | undefined => querySummary.content
+const getContent = (querySummary: DataQuerySummary): DataQuery | undefined => querySummary?.content
 
-const getLabels = (querySummary: DataQuerySummary): Labels => querySummary.props?.labels ?? {}
+const getLabels = (querySummary: DataQuerySummary): Labels => querySummary?.props?.labels ?? {}
 
 const getLabel =
   (lang: LanguageCode) =>
   (querySummary: DataQuerySummary): string =>
     getLabels(querySummary)[lang] ?? ''
 
-const getDescriptions = (querySummary: DataQuerySummary): Labels => querySummary.props?.descriptions ?? {}
+const getDescriptions = (querySummary: DataQuerySummary): Labels => querySummary?.props?.descriptions ?? {}
 
 const getDescription =
   (lang: LanguageCode) =>
