5ad7830ecb174b3b7513b5a36bc1310af7267db0
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-05-08 10:57:49 +0200

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,10 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
+import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
-export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    ignores: ['jsep.ts'],
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
+export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
+  ignores: ['jsep.ts'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
-]
+})
