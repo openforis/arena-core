f5832d8aac876a685f80b37046dbeac8cb42839d
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2024-05-22 16:39:34 +0200

    Expression validator: include analysis attributes (optional) (#226)
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- src/expression/context.ts
+++ src/expression/context.ts
@@ -1,6 +1,7 @@
 export interface ExpressionContext {
   object?: any
   evaluateToNode?: boolean
+  includeAnalysis?: boolean
   /**
    * client timezone offset in minutes as returned by the Date.getTimezoneOffset function
    */
