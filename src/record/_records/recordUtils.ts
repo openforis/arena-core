4b758160baf73876bc90327c1e21b6634c9760ed
Stefano Ricci <SteRiccio@users.noreply.github.com>
2025-04-02 23:13:01 +0200

    code cleanup

--- src/record/_records/recordUtils.ts
+++ src/record/_records/recordUtils.ts
@@ -1,6 +1,6 @@
 import { CategoryItem } from '../../category'
 import { Node, NodeValues } from '../../node'
-import { NodeDef, NodeDefCode, NodeDefs, NodeDefType } from '../../nodeDef'
+import { NodeDef, NodeDefCode, NodeDefEntity, NodeDefs, NodeDefType } from '../../nodeDef'
 import { Survey, Surveys } from '../../survey'
 import { Record } from '../record'
 import {
@@ -12,32 +12,47 @@ import {
   isNodeFilledByUser,
 } from './recordGetters'
 
+const findDependentEnumeratedEntityDefs = (params: { survey: Survey; nodeDef: NodeDef<any> }): NodeDefEntity[] => {
+  const { survey, nodeDef } = params
+  const dependentEnumeratedEntityDefs =
+    NodeDefs.getType(nodeDef) === NodeDefType.code
+      ? Surveys.getDependentEnumeratedEntityDefs({ survey, nodeDef: nodeDef as NodeDefCode })
+      : []
+
+  const applicableDependentEnumeratedEntityDefs = Surveys.findApplicableDependentEnumeratedEntityDefs(nodeDef)(survey)
+  dependentEnumeratedEntityDefs.push(...applicableDependentEnumeratedEntityDefs)
+
+  return dependentEnumeratedEntityDefs
+}
+
+const hasNodeNonEmptyDescendants = (params: {
+  record: Record
+  parentNode: Node | undefined
+  nodeDefDescendant: NodeDefEntity
+}): boolean => {
+  const { record, parentNode, nodeDefDescendant } = params
+  const descendants = getDescendantsOrSelf({
+    record,
+    node: parentNode ?? getRoot(record)!,
+    nodeDefDescendant,
+  })
+  const nonEmptyDescendants = descendants.filter((dependentEntity) =>
+    getChildren(dependentEntity)(record).some((childNode) => isNodeFilledByUser(childNode)(record))
+  )
+  return nonEmptyDescendants.length > 0
+}
+
 export const findDependentEnumeratedEntityDefsNotEmpty =
   (params: { survey: Survey; node: Node; nodeDef: NodeDef<any> }) => (record: Record) => {
     const { survey, node, nodeDef } = params
-    const dependentEnumeratedEntityDefs =
-      NodeDefs.getType(nodeDef) === NodeDefType.code
-        ? Surveys.getDependentEnumeratedEntityDefs({ survey, nodeDef: nodeDef as NodeDefCode })
-        : []
-
-    const applicableDependentEnumeratedEntityDefs = Surveys.findApplicableDependentEnumeratedEntityDefs(nodeDef)(survey)
-    dependentEnumeratedEntityDefs.push(...applicableDependentEnumeratedEntityDefs)
-
+    const dependentEnumeratedEntityDefs = findDependentEnumeratedEntityDefs({ survey, nodeDef })
     if (dependentEnumeratedEntityDefs.length === 0) return []
 
     const parentNode = getParent(node)(record)
 
-    return dependentEnumeratedEntityDefs.filter((dependentEnumeratedEntityDef) => {
-      const dependentEntities = getDescendantsOrSelf({
-        record,
-        node: parentNode ?? getRoot(record)!,
-        nodeDefDescendant: dependentEnumeratedEntityDef,
-      })
-      const dependentEntitiesNotEmpty = dependentEntities.filter((dependentEntity) =>
-        getChildren(dependentEntity)(record).some((childNode) => isNodeFilledByUser(childNode)(record))
-      )
-      return dependentEntitiesNotEmpty.length > 0
-    })
+    return dependentEnumeratedEntityDefs.filter((dependentEnumeratedEntityDef) =>
+      hasNodeNonEmptyDescendants({ record, parentNode, nodeDefDescendant: dependentEnumeratedEntityDef })
+    )
   }
 
 export const getEnumeratingCategoryItems =
