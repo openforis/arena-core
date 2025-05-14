36d4f7df34aedef610e777be709ce98af2b9532a
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2025-05-14 10:55:04 +0200

    Node def taxon: added vernacularNameSelectionKept prop (#271)
    * Node def taxon: added vernacularNameSelectionKept prop
    
    * fixed tests
    
    ---------
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- src/nodeDef/types/taxon.ts
+++ src/nodeDef/types/taxon.ts
@@ -2,6 +2,7 @@ import { NodeDef, NodeDefProps, NodeDefType } from '../nodeDef'
 
 export interface NodeDefTaxonProps extends NodeDefProps {
   taxonomyUuid: string
+  vernacularNameSelectionKept?: boolean
 }
 
 export type NodeDefTaxon = NodeDef<NodeDefType.taxon, NodeDefTaxonProps>
