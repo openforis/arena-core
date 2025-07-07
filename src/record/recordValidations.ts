bc9eb81be16f0fcb74bd003cc9a293e403da70c5
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2025-07-07 13:44:12 +0200

    Record validations: added helper functions (#278)
    * Record validations: added helper functions
    
    * code cleanup

--- src/record/recordValidations.ts
+++ src/record/recordValidations.ts
@@ -2,10 +2,17 @@ import { Validation, Validations } from '../validation'
 
 const prefixValidationFieldChildrenCount = 'childrenCount_'
 
+const isValidationChildrenCountKey = (validationFieldKey: string) =>
+  validationFieldKey?.startsWith(prefixValidationFieldChildrenCount)
+
 const getValidationChildrenCountKey = (params: { nodeParentUuid: string; nodeDefChildUuid: string }): string => {
   const { nodeParentUuid, nodeDefChildUuid } = params
   return `${prefixValidationFieldChildrenCount}${nodeParentUuid}_${nodeDefChildUuid}`
 }
+
+const extractValidationChildrenCountKeyParentUuid = (validationFieldKey: string) => validationFieldKey?.split('_')[1]
+
+const extractValidationChildrenCountKeyNodeDefUuid = (validationFieldKey: string) => validationFieldKey?.split('_')[2]
 
 const getValidationChildrenCount =
   (params: { nodeParentUuid: string; nodeDefChildUuid: string }) =>
@@ -24,6 +31,9 @@ const getValidationNode =
 export const RecordValidations = {
   prefixValidationFieldChildrenCount,
 
+  isValidationChildrenCountKey,
+  extractValidationChildrenCountKeyParentUuid,
+  extractValidationChildrenCountKeyNodeDefUuid,
   getValidationChildrenCountKey,
   getValidationChildrenCount,
   getValidationNode,
