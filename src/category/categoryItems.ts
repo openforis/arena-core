bf6324ea861075d52deae30e16d97af465032d68
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2025-11-14 15:08:55 +0100

    code cleanup

--- src/category/categoryItems.ts
+++ src/category/categoryItems.ts
@@ -3,7 +3,7 @@ import { CategoryItem } from './item'
 
 const getCode = (item: CategoryItem): string => item.props.code ?? ''
 
-const getIndex = (item: CategoryItem): number => item.props.index ?? NaN
+const getIndex = (item: CategoryItem): number => item.props.index ?? Number.NaN
 
 const getLabel = (item: CategoryItem, lang: LanguageCode, defaultToCode?: boolean): string => {
   const label = item.props.labels?.[lang] ?? ''
