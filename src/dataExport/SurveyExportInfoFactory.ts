bb88a0f67a3e49def4a050eab999b11eab207ae6
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-01-12 23:15:00 +0100

    code cleanup

--- src/dataExport/SurveyExportInfoFactory.ts
+++ src/dataExport/SurveyExportInfoFactory.ts
@@ -13,26 +13,8 @@ export type SurveyExportInfoFactoryParams = {
 }
 
 export const SurveyExportInfoFactory: Factory<SurveyExportInfo, SurveyExportInfoFactoryParams> = {
-  createInstance: (params: SurveyExportInfoFactoryParams): SurveyExportInfo => {
-    const {
-      appInfo,
-      exportedByUserUuid,
-      surveyUuid,
-      surveyName,
-      includeData,
-      includeResultAttributes,
-      includeActivityLog,
-    } = params
-
-    return {
-      appInfo,
-      dateExported: new Date(),
-      exportedByUserUuid,
-      surveyUuid,
-      surveyName,
-      includeData: includeData ?? false,
-      includeResultAttributes: includeResultAttributes ?? false,
-      includeActivityLog: includeActivityLog ?? false,
-    }
-  },
+  createInstance: (params: SurveyExportInfoFactoryParams): SurveyExportInfo => ({
+    ...params,
+    dateExported: new Date(),
+  }),
 }
