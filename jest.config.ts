95816fa9ca8767352a897e1e18119275aa6efab0
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-03-24 21:58:56 +0100

    updated jest config

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  transform: { '^.+\\.[tj]sx?$': 'ts-jest' },
+  transformIgnorePatterns: ['/node_modules/(?!change-case/)'],
+  testEnvironment: 'node',
+  testRegex: '/src/.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
+
+export default config
