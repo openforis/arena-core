1f196d13f20a1306cd8179e71ee83c73abca9759
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-03-24 22:03:31 +0100

    updated dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'jest'
 
 const config: Config = {
   transform: { '^.+\\.[tj]sx?$': 'ts-jest' },
-  transformIgnorePatterns: ['/node_modules/(?!change-case/)'],
+  transformIgnorePatterns: ['/node_modules/(?!change-case|uuid/)'],
   testEnvironment: 'node',
   testRegex: '/src/.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
