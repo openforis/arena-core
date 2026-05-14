dad54322b58f4a94fc557b37dbe229adbd9e0c0b
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-05-14 14:27:04 +0200

    code cleanup

--- src/record/recordNodesUpdater/recordNodeDependentsEditableUpdater.ts
+++ src/record/recordNodesUpdater/recordNodeDependentsEditableUpdater.ts
@@ -1,88 +1,19 @@
-import { Node, NodePointer, Nodes } from '../../node'
+import { Nodes } from '../../node'
 import { NodeDefs } from '../../nodeDef'
 import { SurveyDependencyType } from '../../survey/survey'
-import { RecordExpressionEvaluator } from '../recordExpressionEvaluator'
-import { RecordUpdateOptions } from '../records'
-import { getDependentNodePointersByType } from './recordNodesDependentsUpdaterCommons'
 import { RecordNodeDependentsUpdateParams } from './recordNodeDependentsUpdateParams'
+import { updateSelfAndDependentsBoolean } from './recordNodeDependentsBooleanUpdater'
 import { RecordUpdateResult } from './recordUpdateResult'
-
-const expressionEvaluator = new RecordExpressionEvaluator()
-
-const calculateEditableNext = async ({
-  params,
-  updateResult,
-  nodePointer,
-  nodeCtx,
-}: {
-  params: RecordNodeDependentsUpdateParams
-  updateResult: RecordUpdateResult
-  nodePointer: NodePointer
-  nodeCtx: Node
-}): Promise<boolean | undefined> => {
-  const { nodeDef: nodeDefNodePointer } = nodePointer
-  const { uuid: nodeDefUuid } = nodeDefNodePointer
-
-  const editablePrev = Nodes.isChildEditable(nodeCtx, nodeDefUuid)
-  const expressionsToEvaluate = NodeDefs.getEditableIf(nodeDefNodePointer)
-  if (expressionsToEvaluate.length === 0) {
-    if (editablePrev) {
-      return undefined
-    }
-    return true
-  }
-
-  const exprEval = await expressionEvaluator.evalApplicableExpression({
-    ...params,
-    record: updateResult.record,
-    nodeCtx,
-    expressions: expressionsToEvaluate,
-  })
-  return exprEval?.value || false
-}
 
 export const updateSelfAndDependentsEditable = async (
   params: RecordNodeDependentsUpdateParams
 ): Promise<RecordUpdateResult> => {
-  const { survey, record, node, sideEffect = false } = params
-
-  const updateResult = new RecordUpdateResult({ record })
-  const recordUpdateOptions: RecordUpdateOptions = { sideEffect }
-
-  const nodePointersToUpdate = getDependentNodePointersByType({
-    survey,
-    record,
-    node,
+  return updateSelfAndDependentsBoolean({
+    params,
     dependencyType: SurveyDependencyType.editable,
-    includeSelfWhenSourceIsAttribute: true,
-    includeNewEntitySelf: true,
-    includeNewEntityChildPointers: true,
+    getExpressions: ({ nodeDef }) => NodeDefs.getEditableIf(nodeDef),
+    getValuePrev: (nodeCtx, nodeDefUuid) => Nodes.isChildEditable(nodeCtx, nodeDefUuid),
+    assocValue: (nodeCtx, nodeDefUuid, value, sideEffect) =>
+      Nodes.assocChildEditable(nodeCtx, nodeDefUuid, value, sideEffect),
   })
-
-  // NOTE: don't do it in parallel, same nodeCtx metadata could be overwritten
-  for (const nodePointer of nodePointersToUpdate) {
-    const { nodeCtx: nodeCtxNodePointer, nodeDef: nodeDefNodePointer } = nodePointer
-
-    const nodeCtxUuid = nodeCtxNodePointer.uuid
-    const nodeDefUuid = nodeDefNodePointer.uuid
-
-    const nodeCtx = updateResult.getNodeByUuid(nodeCtxUuid) ?? nodeCtxNodePointer
-
-    const editablePrev = Nodes.isChildEditable(nodeCtx, nodeDefUuid)
-    const editable = await calculateEditableNext({
-      params,
-      updateResult,
-      nodePointer,
-      nodeCtx,
-    })
-
-    if (editable === undefined || editablePrev === editable) {
-      continue
-    }
-
-    const nodeCtxUpdated = Nodes.assocChildEditable(nodeCtx, nodeDefUuid, editable, sideEffect)
-    updateResult.addNode(nodeCtxUpdated, recordUpdateOptions)
-  }
-
-  return updateResult
 }
--- src/record/recordNodesUpdater/recordNodeDependentsVisibleUpdater.ts
+++ src/record/recordNodesUpdater/recordNodeDependentsVisibleUpdater.ts
@@ -1,88 +1,19 @@
-import { Node, NodePointer, Nodes } from '../../node'
+import { Nodes } from '../../node'
 import { NodeDefs } from '../../nodeDef'
 import { SurveyDependencyType } from '../../survey/survey'
-import { RecordExpressionEvaluator } from '../recordExpressionEvaluator'
-import { RecordUpdateOptions } from '../records'
-import { getDependentNodePointersByType } from './recordNodesDependentsUpdaterCommons'
 import { RecordNodeDependentsUpdateParams } from './recordNodeDependentsUpdateParams'
+import { updateSelfAndDependentsBoolean } from './recordNodeDependentsBooleanUpdater'
 import { RecordUpdateResult } from './recordUpdateResult'
-
-const expressionEvaluator = new RecordExpressionEvaluator()
-
-const calculateVisibleNext = async ({
-  params,
-  updateResult,
-  nodePointer,
-  nodeCtx,
-}: {
-  params: RecordNodeDependentsUpdateParams
-  updateResult: RecordUpdateResult
-  nodePointer: NodePointer
-  nodeCtx: Node
-}): Promise<boolean | undefined> => {
-  const { nodeDef: nodeDefNodePointer } = nodePointer
-  const { uuid: nodeDefUuid } = nodeDefNodePointer
-
-  const visiblePrev = Nodes.isChildVisible(nodeCtx, nodeDefUuid)
-  const expressionsToEvaluate = NodeDefs.getVisibleIf(nodeDefNodePointer)
-  if (expressionsToEvaluate.length === 0) {
-    if (visiblePrev) {
-      return undefined
-    }
-    return true
-  }
-
-  const exprEval = await expressionEvaluator.evalApplicableExpression({
-    ...params,
-    record: updateResult.record,
-    nodeCtx,
-    expressions: expressionsToEvaluate,
-  })
-  return exprEval?.value || false
-}
 
 export const updateSelfAndDependentsVisible = async (
   params: RecordNodeDependentsUpdateParams
 ): Promise<RecordUpdateResult> => {
-  const { survey, record, node, sideEffect = false } = params
-
-  const updateResult = new RecordUpdateResult({ record })
-  const recordUpdateOptions: RecordUpdateOptions = { sideEffect }
-
-  const nodePointersToUpdate = getDependentNodePointersByType({
-    survey,
-    record,
-    node,
+  return updateSelfAndDependentsBoolean({
+    params,
     dependencyType: SurveyDependencyType.visible,
-    includeSelfWhenSourceIsAttribute: true,
-    includeNewEntitySelf: true,
-    includeNewEntityChildPointers: true,
+    getExpressions: ({ nodeDef }) => NodeDefs.getVisibleIf(nodeDef),
+    getValuePrev: (nodeCtx, nodeDefUuid) => Nodes.isChildVisible(nodeCtx, nodeDefUuid),
+    assocValue: (nodeCtx, nodeDefUuid, value, sideEffect) =>
+      Nodes.assocChildVisible(nodeCtx, nodeDefUuid, value, sideEffect),
   })
-
-  // NOTE: don't do it in parallel, same nodeCtx metadata could be overwritten
-  for (const nodePointer of nodePointersToUpdate) {
-    const { nodeCtx: nodeCtxNodePointer, nodeDef: nodeDefNodePointer } = nodePointer
-
-    const nodeCtxUuid = nodeCtxNodePointer.uuid
-    const nodeDefUuid = nodeDefNodePointer.uuid
-
-    const nodeCtx = updateResult.getNodeByUuid(nodeCtxUuid) ?? nodeCtxNodePointer
-
-    const visiblePrev = Nodes.isChildVisible(nodeCtx, nodeDefUuid)
-    const visible = await calculateVisibleNext({
-      params,
-      updateResult,
-      nodePointer,
-      nodeCtx,
-    })
-
-    if (visible === undefined || visiblePrev === visible) {
-      continue
-    }
-
-    const nodeCtxUpdated = Nodes.assocChildVisible(nodeCtx, nodeDefUuid, visible, sideEffect)
-    updateResult.addNode(nodeCtxUpdated, recordUpdateOptions)
-  }
-
-  return updateResult
 }
