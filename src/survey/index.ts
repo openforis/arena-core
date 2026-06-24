e36294850d8a5ee2166a939b59e2479fc493591b
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-06-24 22:10:23 +0200

    added survey file types

--- src/survey/index.ts
+++ src/survey/index.ts
@@ -1,15 +1,31 @@
 export { NodeDefsFixer } from './nodeDefsFixer'
+export { SurveyDocImages } from './surveyDocImages'
+export { SurveyFileFactory } from './surveyFileFactory'
+export { SurveyDocImageFactory } from './surveyDocFileFactory'
+export type { SurveyFileFactoryParams } from './surveyFileFactory'
+export type { SurveyDocImageFactoryParams } from './surveyDocFileFactory'
 
 export type {
   Survey,
   SurveyCycle,
   SurveyDependencyGraph,
   SurveyDependency,
+  SurveyDocImage,
+  SurveyDocImageProps,
+  SurveyFile,
+  SurveyFileProps,
   SurveyProps,
   SurveySecurity,
 } from './survey'
 
-export { defaultCycle, SurveyDependencyType, SurveySecurityProp, surveySecurityDefaults } from './survey'
+export {
+  defaultCycle,
+  SurveyDependencyType,
+  SurveyDocPlace,
+  SurveySecurityProp,
+  surveyDocImagePropKeys,
+  surveySecurityDefaults,
+} from './survey'
 
 export { Surveys } from './surveys'
 
---
+++ src/survey/surveyFileFactory.ts
@@ -0,0 +1,29 @@
+import { Factory } from '../common'
+import { Dates, UUIDs } from '../utils'
+import { SurveyFile, SurveyFileProps } from './survey'
+
+export type SurveyFileFactoryParams = {
+  name?: SurveyFileProps['name']
+  labels?: SurveyFileProps['labels'] | null
+  size?: SurveyFileProps['size']
+  type?: SurveyFileProps['type']
+  temporary?: SurveyFileProps['temporary']
+}
+
+export const SurveyFileFactory: Factory<SurveyFile, SurveyFileFactoryParams> = {
+  createInstance: (params: SurveyFileFactoryParams = {}): SurveyFile => {
+    const { name, labels, size, type, temporary = false } = params
+
+    return {
+      uuid: UUIDs.v4(),
+      dateCreated: Dates.nowFormattedForStorage(),
+      props: {
+        name,
+        labels: labels ?? undefined,
+        size,
+        type,
+        temporary,
+      },
+    }
+  },
+}
