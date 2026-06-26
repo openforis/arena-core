c2aa9fe9d564ddc574e0970beaac233102bf5986
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2026-06-26 10:54:49 +0200

    Validations: added nulls check (#376)
    * Validations: added nulls check
    
    * code cleanup

--- src/validation/validationResults.ts
+++ src/validation/validationResults.ts
@@ -1,23 +1,23 @@
 import { Labels, LanguageCode } from '../language'
 import { ValidationResult, ValidationSeverity } from './validation'
 
-const getKey = (result: ValidationResult): string | undefined => result.key
+const getKey = (result: ValidationResult): string | undefined => result?.key
 
-const getParams = (result: ValidationResult): { [key: string]: any } | undefined => result.params
+const getParams = (result: ValidationResult): { [key: string]: any } | undefined => result?.params
 
-const getSeverity = (result: ValidationResult): ValidationSeverity | undefined => result.severity
+const getSeverity = (result: ValidationResult): ValidationSeverity | undefined => result?.severity
 
-const getMessages = (result: ValidationResult): Labels | undefined => result.messages
+const getMessages = (result: ValidationResult): Labels | undefined => result?.messages
 
 const getMessage =
   (lang: LanguageCode) =>
   (result: ValidationResult): string | undefined =>
-    result.messages?.[lang] ?? Object.values(result.messages ?? {})[0] // fallback to first message if the requested language is not available
+    result?.messages?.[lang] ?? Object.values(result?.messages ?? {})[0] // fallback to first message if the requested language is not available
 
 const hasMessages = (result: ValidationResult): boolean =>
-  result.messages != null && Object.keys(result.messages).length > 0
+  result?.messages != null && Object.keys(result.messages).length > 0
 
-const isError = (result: ValidationResult): boolean => result.severity === ValidationSeverity.error
+const isError = (result: ValidationResult): boolean => result?.severity === ValidationSeverity.error
 
 export const ValidationResults = {
   getKey,
